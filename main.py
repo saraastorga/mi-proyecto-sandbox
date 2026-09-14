4babfdca5dc9e2a3d5181994a4ccf210b39a9420
Sara Astorga <sara.astorga24@estudiantes.uva.es>
2026-09-14 10:12:14 +0200

    feat: Implementa la función de lectura de datos

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+
+def leer_datos(fichero):
+    """Lee números enteros de un fichero, uno por línea."""
+    # Tu código aquí
+    pass
+
+if __name__ == "__main__":
+    # Tu código para visualizar la lista de valores leídos aquí
+    pass
